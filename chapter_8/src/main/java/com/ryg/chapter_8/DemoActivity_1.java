b2b73868e599c7684fffe0bcce8702863ec632a9
baidng <baidng@163.com>
2021-08-14 00:36:52 +0800

    修改第8章的bug

--- chapter_8/src/main/java/com/ryg/chapter_8/DemoActivity_1.java
+++ chapter_8/src/main/java/com/ryg/chapter_8/DemoActivity_1.java
@@ -4,19 +4,29 @@ import com.ryg.chapter_8.R;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DemoActivity_1 extends Activity {
     private static final String TAG = "DemoActivity_1";
+
+    int OVERLAY_PERMISSION_CODE = 100;
+    boolean askedForOverlayPermission;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo_1);
-        initView();
     }
 
     private void initView() {
@@ -24,8 +34,19 @@ public class DemoActivity_1 extends Activity {
         TextView textView = new TextView(this);
         textView.setText("this is toast!");
         dialog.setContentView(textView);
-        dialog.getWindow().setType(LayoutParams.TYPE_SYSTEM_ERROR);
+        int type = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            type = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+        dialog.getWindow().setType(type);
         dialog.show();
     }
+
+    public void onclick(View v){
+        initView();
+    }
+
 
 }
